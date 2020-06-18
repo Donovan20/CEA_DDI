1e42e088043e47dae7882d732bae1bc5efee4181
Donovan Avila <aavila10@alumnos.uaq.mx>
2020-06-18 13:34:32 -0500

    Django project

---
+++ cea_ddi/app/ddi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DdiConfig(AppConfig):
+    name = 'ddi'
---
+++ cea_ddi/cea_ddi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cea_ddi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cea_ddi.settings')
+
+application = get_asgi_application()
