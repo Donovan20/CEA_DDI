c35979ed5c5070807aa625dddb288ffd51a0ed16
Donovan Avila <aavila10@alumnos.uaq.mx>
2020-08-02 22:05:28 -0500

    Instalador

--- instalar.bat
+++ instalar.bat
@@ -2,4 +2,5 @@ pip install -r requerimientos.txt
 cd cea_ddi
 python manage.py makemigrations ddi
 python manage.py migrate
-django-admin loaddata datos_iniciales
+manage.py loaddata datos_iniciales.json
+manage.py createsuperuser
